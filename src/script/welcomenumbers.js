3729abcfbd99668410392c216e297102d0c23e11
R_K <shawnfroste@outlook.fr>
2024-12-17 21:26:24 +0100

    Add class and randomly shine instead of hover

--- src/script/welcomenumbers.js
+++ src/script/welcomenumbers.js
@@ -4,5 +4,19 @@ let splitNum = numString.split("");
 
 numbers.textContent = "";
 for (let i = 0; i < splitNum.length; i++) {
-    numbers.innerHTML += "<span>"+splitNum[i]+"</span>"
+    numbers.innerHTML += "<span>" + splitNum[i] + "</span>";
 }
+
+function randomShine() {
+    let spans = document.querySelectorAll(".binaryNumber span");
+    let randomIndex = Math.floor(Math.random() * spans.length); 
+
+    spans[randomIndex].classList.add("shine"); 
+
+
+    setTimeout(() => {
+        spans[randomIndex].classList.remove("shine");
+    }, 1000); 
+}
+
+setInterval(randomShine, 200);
